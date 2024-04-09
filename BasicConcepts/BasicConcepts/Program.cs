8d5b4e0f981652e5565e266ed1301c6c1a232975
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-09 14:37:31 +0600

    array

--- BasicConcepts/BasicConcepts/Program.cs
+++ BasicConcepts/BasicConcepts/Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace BasicConcepts;
 
 using System;
+using System.ComponentModel.Design.Serialization;
 
 class Program
 {
-    static void Main(string[] args){
+    static void Main(string[] args)
+    {
 
         /* 
         
@@ -43,6 +45,46 @@ class Program
         else {
             Console.WriteLine("You are a kid");
         } */
+
+        /* 
+
+        Array
+
+        int[] rolls = new int[3];
+
+        Console.WriteLine("Enter student rolls : ");
+
+        for(int i=0; i < rolls.Length; i++) {
+            Console.WriteLine($"Enter roll number {i+1}");
+            rolls[i] = int.Parse(Console.ReadLine());
+        }
+
+        foreach(int roll in rolls) {
+            Console.Write("{0} ", roll);
+        }
+
+        Console.WriteLine("");
+
+
+        string[] names = new string[] {"Mugdho","Mila","Meghla"};
+
+        foreach(string name in names) {
+            Console.WriteLine($"{name}");
+        }
+
+
+        object[] mixed = new object[] {"text",85,true};
+
+        var employees = new[] {"name", "good"};
+        var codes = new [] {4,5};
+
+        string[,] customers = new[,] {
+            {"Mugdho","Mahmud"},
+            {"Mila","Mahmud"},
+        };
+
+        }
+ */
 
     }
 }

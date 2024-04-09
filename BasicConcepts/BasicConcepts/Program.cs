4bf070408363dee585ba65f5b2b741408b0120f4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-04-09 14:06:09 +0600

    added max and min value of data types

--- BasicConcepts/BasicConcepts/Program.cs
+++ BasicConcepts/BasicConcepts/Program.cs
@@ -6,5 +6,25 @@ class Program
 {
     static void Main(string[] args){
         Console.WriteLine("Hello World");
+        
+        Console.WriteLine("max value of int is {0}",int.MaxValue);
+        Console.WriteLine("min value of int is {0}",int.MinValue);
+
+        Console.WriteLine("max value of long is {0}",long.MaxValue);
+        Console.WriteLine("min value of long is {0}",long.MinValue);
+
+        Console.WriteLine("max value of float is {0}",float.MaxValue);
+        Console.WriteLine("min value of float is {0}",float.MinValue);
+
+        Console.WriteLine("max value of double is {0}",double.MaxValue);
+        Console.WriteLine("min value of double is {0}",double.MinValue);
+
+        Console.WriteLine("max value of decimal is {0}",decimal.MaxValue);
+        Console.WriteLine("min value of decimal is {0}",decimal.MinValue);
+
+
+        
+
     }
 }
+

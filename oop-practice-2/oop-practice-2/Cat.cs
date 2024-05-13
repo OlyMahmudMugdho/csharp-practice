1fef8cd16406fb6ab7cad062f54b7724ebb426d5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-13 09:54:19 +0600

    overriden in cat

--- oop-practice-2/oop-practice-2/Cat.cs
+++ oop-practice-2/oop-practice-2/Cat.cs
@@ -3,4 +3,9 @@ namespace OopTwo;
 public class Cat : Animal
 {
     public bool isDomestic;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"meow.... Name : {this.Name}, Legs : {this.Legs}");
+    }
 }

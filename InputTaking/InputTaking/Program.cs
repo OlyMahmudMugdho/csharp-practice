ea1813e41585a639f31daef735a7c0e67584941d
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-09-22 10:52:18 +0600

    Add files via upload

---
+++ InputTaking/InputTaking/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InpuTaking
+{
+    class InputTaking
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi, {name}");
+
+            Console.WriteLine("Enter your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is : " + age);
+        }
+    }
+}

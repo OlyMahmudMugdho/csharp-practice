1b7e00e1a8078ea0672cf78703e6d9319217c04b
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-09-22 11:17:47 +0600

    fields and property

---
+++ FieldAndProperty/Program.cs
@@ -0,0 +1,20 @@
+﻿using FieldAndProperty;
+
+namespace FieldProperty
+{
+    class Practice
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.SetName("car");
+            Console.WriteLine(vehicle.GetName());
+
+            vehicle.Wheels = 4;
+            Console.WriteLine(vehicle.Wheels);
+
+            vehicle.Description = "a new car";
+            Console.WriteLine(vehicle.Description);
+        }
+    }
+}
---
+++ FieldAndProperty/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldAndProperty
+{
+    class Vehicle
+    {
+        private string _name;
+        private int _wheels;
+        private string _description;
+
+        public void SetName(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetName() {
+            return this._name;
+        }
+
+        public int Wheels
+        {
+            get { return this._wheels; }
+            set { this._wheels = value; }
+        }
+
+        public string Description{ get; set; }
+    }
+}

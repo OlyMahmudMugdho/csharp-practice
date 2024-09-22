5ab1b9344ba1b5d3527a93010d5d9130d069e1d4
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-09-22 10:45:17 +0600

    Jagged Array

---
+++ JaggedArray/JaggedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays
+{
+    class JaggedArray
+    {
+        public static void Main(string[] args)
+        {
+            int[][] nums = new int[2][];
+            nums[0] = new int[2];
+            nums[1] = new int[2];
+
+            nums[0] = new int[] { 7, 6, 9, 10 };
+            nums[1] = new int[] { 8, 10, 3, 4 };
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums[i].Length; j++)
+                {
+                    Console.Write(nums[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

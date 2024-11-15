6842ef2f34dc2bd467dfab7704251fe7e215e1ce
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-15 11:27:45 +0600

    LINQ : Where

---
+++ linq/FilteringUsingWhere/FilteringUsingWhere/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FilteringWithWhere;
+
+class Filtering
+{
+    public static void Main(string[] args)
+    {
+        List<int> nums = new List<int>() { 5,3,4,9,2};
+        var largeNums =  nums.Where(num => num >= 4);
+
+        foreach (int num in largeNums)
+        {
+            Console.Write($"{num} ");
+        }
+    }
+}

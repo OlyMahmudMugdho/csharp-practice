d3cfaf43c529377f11dd515c8f22dc907c2c122d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-11-16 21:16:36 +0600

    select

---
+++ linq/select/select/Person.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Person
+{
+	public string? Name { get; set;}
+	public int? Age { get; set;}
+
+}
---
+++ linq/select/select/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace select;
+public class Selecting
+{
+    public static void Main(string[] args)
+    {
+        List<Person> list = new List<Person>()
+        {
+            new Person{Name = "Mugdho", Age = 24},
+            new Person{Name = "Mila",  Age = 23}
+        };
+
+        var allPerson = list.Select(p => p.Name);
+
+        foreach(var person in allPerson)
+        {
+            Console.Write($"{person}  ");
+        }
+    }
+}
